5d9b18fde5f93dd49ffe10f68bafe707a01c9e1f
Vuong Nguyen <vuong.nguyen@linksinternational.local>
2022-05-12 08:03:52 +0800

    Add Home.vue and form component

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,10 +1,20 @@
+import * as VueRouter from 'vue-router'
+import { createApp } from 'vue'
+
+const Home = import('../views/HomePage.vue')
+
 const routes = [
-	{ path: '/', component: () => import('../views/Home') },
+	{ path: '/', component: Home },
 ];
 
 const router = VueRouter.createRouter({
 	history: VueRouter.createWebHashHistory(),
 	routes,
 });
+
+const app = createApp({})
+
+app.use(router)
+app.mount('#app')
 
 export default router;
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,45 @@
+<script setup>
+    import { ref } from 'vue';
+    import url from '../api/url';
+    
+    const input = ref('');
+    const fill = ref(true);
+
+    let shortenedUrl = '';
+
+    const baseHost = 'http://localhost.com/';
+
+    const handleShortenUrl = (inputURL) => {
+        url.getURLs(inputURL).then(
+            (res) => {
+                console.log(res);
+                shortenedUrl = `${baseHost}${res || '1234'}`;
+            }
+        );
+    };
+
+    
+</script>
+
+<template>
+    <div id="home">
+        <el-space :fill="fill" :size="200" wrap>
+            <el-card shadow="always" class="box-card">
+                <el-input v-model="input" size="large" placeholder="Enter the URL for shorten" clearable />
+                <p><el-link type="primary" v-model="shortenedUrl"></el-link></p>
+                <el-button type="primary" size="large" @click="handleShortenUrl">Shorten</el-button>
+            </el-card>
+        </el-space>
+    </div>
+</template>
+
+<style scoped>
+    #home {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
